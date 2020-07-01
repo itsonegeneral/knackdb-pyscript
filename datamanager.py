d9aba359a6e93d6ef6eeb84efc91c613e0515121
rithiks <srithik1999@gmail.com>
2020-07-01 20:13:34 +0530

    initial

--- datamanager.py
+++ datamanager.py
@@ -1,51 +1,36 @@
 import pyrebase
 from datetime import datetime
+from datetime import timedelta
 import time
 
 config = {
-  "apiKey": "AIzaSyBQrLQYglhDTJ4LgNBKWfphbdrvyWjRViY",
-  "authDomain": "knacktestmodule.firebaseapp.com",
-  "databaseURL": "https://knacktestmodule.firebaseio.com/",
-  "storageBucket": "knacktestmodule.appspot.com"
+  "apiKey": "AIzaSyBAB3_BSepybVEPVR-PNcHxGC9aVOqT3uk",
+  "authDomain": "knack-quiz-b57d5.firebaseapp.com",
+  "databaseURL": "https://knack-quiz-b57d5.firebaseio.com/",
+  "storageBucket": "knack-quiz-b57d5.appspot.com"
 }
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-datetime.now()
-time_format = '%Y-%m-%d %H:%M:%S'
-
-
-def upload_data(time_stamp):
-    data = {
-        "name": "rithik",
-        "time": time_stamp
-    }
-    db.child('python').child(db.generate_key()).set(data)
+time_format = '%c'
+print(datetime.now().strftime(time_format))
 
 def database_check():
     print('\nStarting Database Inspect')
-    data = db.child('python').get()
-    try:
-        for user in data.each():
-            k = user.val()
-            data_time_stamp = datetime.strptime(k['time'],time_format)
-            time_string = datetime.now().strftime(time_format);
-            current_time_stamp = datetime.strptime(str(time_string),time_format)
-            time_difference = current_time_stamp - data_time_stamp
-            if(time_difference.seconds>600):
-                print('Post older than 10 minute found deleting post id ' + user.key())
-                db.child('python').child(user.key()).remove()
-    except:
-        print('No data found')        
-
+    data = db.child('online_players').get()
+    for contest in data.each():
+        k = contest.val()
+        start_time_db = datetime.strptime(k['lastOnline'],time_format)
+        current_time = datetime.now().strftime(time_format)
+        parsed = datetime.strptime(current_time,time_format)
+        time_difference = parsed - start_time_db
+        print(time_difference.seconds)
+        if(time_difference.seconds>30):
+            print('User older than 30 Second found deleting id ' + contest.key())
+            db.child('online_players').child(contest.key()).remove() 
 
 while True:
     database_check()
-    time.sleep(10)
+    time.sleep(5)
 
-def start_upload_process():
-    x=0
-    for x in range(100):
-        print('Uploaded data with timestamp' + str(datetime.now()))
-        upload_data(str(datetime.now().strftime(time_format)))
-        time.sleep(2)
+
